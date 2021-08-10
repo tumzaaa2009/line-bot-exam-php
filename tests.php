666f8dc1531c3bcce77bf0d9f2fd8755b85b52cc
tumzaaa2009 <48194538+tumzaaa2009@users.noreply.github.com>
2021-08-10 13:51:53 +0700

    Create tests.php

---
+++ tests.php
@@ -0,0 +1,44 @@
+<?php
+ $LINEData = file_get_contents('php://input');
+ $jsonData = json_decode($LINEData,true);
+ $replyToken = $jsonData["events"][0]["replyToken"];
+ $text = $jsonData["events"][0]["message"]["text"];
+ 
+ 
+ 
+ function sendMessage($replyJson, $token){
+   $ch = curl_init($token["URL"]);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+   curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+   curl_setopt($ch, CURLOPT_POST, true);
+   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+       'Content-Type: application/json',
+       'Authorization: Bearer ' . $token["AccessToken"])
+       );
+   curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
+   $result = curl_exec($ch);
+   curl_close($ch);
+return $result;
+}
+ 
+//  $getUser = $mysql->query("SELECT * FROM `test` WHERE `question`='$text'");
+//  $getuserNum = $getUser->num_rows;
+ 
+ 
+     $message = '{
+     "type" : "text",
+     "text" : "ไม่มีข้อมูลที่ต้องการ"
+     }';
+     $replymessage = json_decode($message);
+//ค่าที่ส่งไป
+ 
+ $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
+ $lineData['AccessToken'] = "4+wlVCk7j87dsSACquSCsDRO/jDtWmE+zus83z1OarXrxvAQEUUIuVuTL5V0f0zSWWH0LB5kkeWr23DWuUMDqA40QimgrSSr1ljJP4+SHgsF+mQepYcSyIzyTMMYFdhN3txmcZBeaHXX8bt6yZMH5AdB04t89/1O/w1cDnyilFU=";
+ $replyJson["replyToken"] = $replyToken;
+ $replyJson["messages"][0] = $replymessage;
+ 
+ $encodeJson = json_encode($replyJson);
+ 
+ $results = sendMessage($encodeJson,$lineData);
+ echo $results;
+ http_response_code(200);
