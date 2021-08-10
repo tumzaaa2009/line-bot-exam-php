5db1509e67ded1b645d8ee24d8930a84f310fe1a
tumzaaa2009 <48194538+tumzaaa2009@users.noreply.github.com>
2021-08-10 11:33:37 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php 
  /*Return HTTP Request 200*/
  http_response_code(200);
-file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
 {"events":[{"type":"message","replyToken":"7d7d02d64b954dfcb93683de8cc41f5f","source":{"userId":"U1a81dc36b7c95042c6ee4718cd5a7c18","type":"user"},"timestamp":1552505328709,"message":{"type":"text","id":"9509179836165","text":"ทดสอบ"}}],"destination":"Uf34ef2c60c48fb450b5abba7bda444cf"}
+file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
+
 ?>
