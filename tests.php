68930fe6448e3ba24f1201145e3739fda6af2b64
tumzaaa2009 <48194538+tumzaaa2009@users.noreply.github.com>
2021-08-10 15:09:22 +0700

    Update tests.php

--- tests.php
+++ tests.php
@@ -29,40 +29,7 @@ return $result;
      "type" : "text",
      "text" : "ไม่มีข้อมูลที่ต้องการ"
      }';
- }else if($text=="จองห้องประชุม"){
-       $message = '{
-  "type": "template",
-  "altText": "this is a image carousel template",
-  "template": {
-      "type": "image_carousel",
-      "columns": [
-          {
-            "imageUrl": "https://example.com/bot/images/item1.jpg",
-            "action": {
-              "type": "postback",
-              "label": "Buy",
-              "data": "action=buy&itemid=111"
-            }
-          },
-          {
-            "imageUrl": "https://example.com/bot/images/item2.jpg",
-            "action": {
-              "type": "message",
-              "label": "Yes",
-              "text": "yes"
-            }
-          },
-          {
-            "imageUrl": "https://example.com/bot/images/item3.jpg",
-            "action": {
-              "type": "uri",
-              "label": "View detail",
-              "uri": "http://example.com/page/222"
-            }
-          }
-      ]
-  }
-}';
+ }
 
 
   $replymessage = json_decode($message);//ค่าที่ส่งไป
@@ -76,3 +43,4 @@ return $result;
  $results = sendMessage($encodeJson,$lineData);
  echo $results;
  http_response_code(200);
+?>
