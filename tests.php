813c46f8516e6165f60b36f02c3cee9f2b0be110
tumzaaa2009 <48194538+tumzaaa2009@users.noreply.github.com>
2021-08-10 15:12:12 +0700

    Update tests.php

--- tests.php
+++ tests.php
@@ -29,6 +29,40 @@ return $result;
      "type" : "text",
      "text" : "ไม่มีข้อมูลที่ต้องการ"
      }';
+ }else if ($text =="จองห้อง"){
+ {
+  "type": "template",
+  "altText": "this is a image carousel template",
+  "template": {
+      "type": "image_carousel",
+      "columns": [
+          {
+            "imageUrl": "https://example.com/bot/images/item1.jpg",
+            "action": {
+              "type": "postback",
+              "label": "Buy",
+              "data": "action=buy&itemid=111"
+            }
+          },
+          {
+            "imageUrl": "https://example.com/bot/images/item2.jpg",
+            "action": {
+              "type": "message",
+              "label": "Yes",
+              "text": "yes"
+            }
+          },
+          {
+            "imageUrl": "https://example.com/bot/images/item3.jpg",
+            "action": {
+              "type": "uri",
+              "label": "View detail",
+              "uri": "http://example.com/page/222"
+            }
+          }
+      ]
+  }
+}
  }
 
 
