c07268fa784513f4736af5e15791f7c8e5aefbca
tumzaaa2009 <48194538+tumzaaa2009@users.noreply.github.com>
2021-08-10 13:30:53 +0700

    Update test.php

--- test.php
+++ test.php
@@ -19,7 +19,7 @@
 	$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
   	$LINEDatas['token'] = "4+wlVCk7j87dsSACquSCsDRO/jDtWmE+zus83z1OarXrxvAQEUUIuVuTL5V0f0zSWWH0LB5kkeWr23DWuUMDqA40QimgrSSr1ljJP4+SHgsF+mQepYcSyIzyTMMYFdhN3txmcZBeaHXX8bt6yZMH5AdB04t89/1O/w1cDnyilFU=";
 
-  	$results = sentMessage($encodeJson,$replyToken);
+  	$results = sentMessage($encodeJson,$LINEDatas);
 
 	/*Return HTTP Request 200*/
 	http_response_code(200);
