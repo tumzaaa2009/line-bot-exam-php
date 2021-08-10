28fcf6765d292c6de4b6899a93084cc484ad9504
tumzaaa2009 <48194538+tumzaaa2009@users.noreply.github.com>
2021-08-10 14:13:33 +0700

    Update tests.php

--- tests.php
+++ tests.php
@@ -27,39 +27,14 @@ return $result;
      }';
  }else {
        $message = '{
-  "type": "template",
-  "altText": "This is a buttons template",
-  "template": {
-      "type": "buttons",
-      "thumbnailImageUrl": "https://example.com/bot/images/image.jpg",
-      "imageAspectRatio": "rectangle",
-      "imageSize": "cover",
-      "imageBackgroundColor": "#FFFFFF",
-      "title": "Menu",
-      "text": "Please select",
-      "defaultAction": {
-          "type": "uri",
-          "label": "View detail",
-          "uri": "http://example.com/page/123"
-      },
-      "actions": [
-          {
-            "type": "postback",
-            "label": "Buy",
-            "data": "action=buy&itemid=123"
-          },
-          {
-            "type": "postback",
-            "label": "Add to cart",
-            "data": "action=add&itemid=123"
-          },
-          {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://example.com/page/123"
-          }
-      ]
-  }
+  "type": "button",
+  "action": {
+    "type": "uri",
+    "label": "Tap me",
+    "uri": "https://example.com"
+  },
+  "style": "primary",
+  "color": "#0000ff"
 }';
  }
 
