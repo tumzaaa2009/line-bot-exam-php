dcd14da9a8c153d6b27ca4667c77237e3b06d927
tumzaaa2009 <48194538+tumzaaa2009@users.noreply.github.com>
2021-08-11 13:16:30 +0700

    Update tests.php

--- tests.php
+++ tests.php
@@ -48,16 +48,6 @@ return $result;
       },
       "actions": [
           {
-            "type": "postback",
-            "label": "Buy",
-            "data": "action=buy&itemid=123"
-          },
-          {
-            "type": "postback",
-            "label": "Add to cart",
-            "data": "action=add&itemid=123"
-          },
-          {
             "type": "uri",
             "label": "View detail",
             "uri": "http://example.com/page/123"
